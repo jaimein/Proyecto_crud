4d673962aadbdff756a5818fd11ef921dc230747
ubu <ubu@ubu-portatil>
2016-01-11 07:55:52 +0100

    index.php realizado y edita.php falta la parte de php

---
+++ edita.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // coger el parámetro que nos permitirá identificar el registro
+        // isset() es una función PHP usado para verificar si una variable tiene valor o no
+        $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Registro no encontrado.');
+        include 'conexion.php';
+        // leer el registro de la tabla
+        $query = "SELECT nif, nombre, apellido1, apellido2, email, telefono, usuario, password "
+        . "FROM clientes  "
+        . "WHERE id = ? ";
+        if ($stmt = $conexion->prepare($query)) {
+        // inicializamos el parámetro
+            $stmt->bind_param('d', $id);
+        // ejecutamos la consulta
+            $stmt->execute();
+            $stmt->bind_result($nif, $nombre, $apellido1, $apellido2, $email, $telefono, $usuario, $password);
+        // recuperamos la variable
+            $stmt->fetch();
+        }   
+        ?>
+        <form action='edita.php?id=<?php echo htmlspecialchars($id); ?>' method='post' border='0'>
+            <table>
+                <tr>
+                    <td>NIF:</td>
+                    <td><input type='text' name='nif' value="<?php echo htmlspecialchars($nif, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td>Nombre:</td>
+                    <td><input type='text' name='nombre' value="<?php echo htmlspecialchars($nombre, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td>1er Apellido</td>
+                    <td><input type="text" name='apellido1' value="<?php echo htmlspecialchars($apellido1, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td>2º Apellido</td>
+                    <td><input type='text' name='apellido2' value="<?php echo htmlspecialchars($apellido2, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td>email</td>
+                    <td><input type="text" name='email' value="<?php echo htmlspecialchars($email, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td>Telefono</td>
+                    <td><input type='text' name='telefono' value="<?php echo htmlspecialchars($telefono, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td>Usuario</td>
+                    <td><input type="text" name='usuario' value="<?php echo htmlspecialchars($usuario, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type='text' name='password' value="<?php echo htmlspecialchars($password, ENT_QUOTES); ?>" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Guardar' />
+                        <a href='index.php'>Volver a Inicio</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -10,9 +10,61 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <h1>Listado de Clientes</h1>
         <?php
-        echo "Este es el directorio del proyecto del crud";
-        // put your code here
+            // incluir la conexión a la base de datos
+            include 'conexion.php';
+            // Elegir los datos que deseamos recuperar de la tabla
+            $query = "SELECT id, nif, nombre, apellido1, apellido2, email, telefono, usuario  "
+                . "FROM clientes "
+                . "ORDER BY apellido1, apellido2, nombre";
+            if ($stmt = $conexion->prepare($query)) {
+                if (!$stmt->execute()) {
+                    die('Error de ejecución de la consulta. ' . $conexion->error);
+                }
+            // recoger los datos
+            $stmt->bind_result($id, $nif, $nombre, $apellido1, $apellido2, $email, $telefono, $usuario);
+            // enlace a alta de cliente
+            echo "<div>";
+            echo "<a href='altas.php'>Alta cliente</a>";
+            echo "</div>";
+            //cabecera de los datos mostrados
+            echo "<table>"; //start table
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>NIF</th>";
+            echo "<th>Nombre</th>";
+            echo "<th>Apellido 1</th>";
+            echo "<th>Apellido 2</th>";
+            echo "<th>email</th>";
+            echo "<th>telefono</th>";
+            echo "<th>usuario</th>";
+            echo "</tr>";
+            //recorrido por el resultado de la consulta
+            while ($stmt->fetch()) {
+                echo "<tr>";
+                echo "<td>$nif</td>";
+                echo "<td>$nombre</td>";
+                echo "<td>$apellido1</td>";
+                echo "<td>$apellido2</td>";
+                echo "<td>$email</td>";
+                echo "<td>$telefono</td>";
+                echo "<td>$usuario</td>";
+                echo "<td>";
+                // Este enlace es para modificar el registro
+                echo "<a href='edita.php?id={$id}'>Edita</a>";
+                echo " / ";
+                // Este enlace es para borrar el registro y también se explicará más tarde
+                echo "<a href='javascript:borra_cliente(\"$id\")'> Elimina </a>";
+                echo "</td>";
+                echo "</tr>\n";
+            }
+            // end table
+            echo "</table>";
+            $stmt->close();
+            } else {
+                die('Imposible preparar la consulta. ' . $conexion->error);
+            }
         ?>
     </body>
 </html>
