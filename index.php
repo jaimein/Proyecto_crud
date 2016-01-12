48cd3780155553bf15b8839c2edb372477f189e4
Juanjo <Juanjo@Juanjo-PC>
2016-01-12 12:51:54 +0100

    Actualizacion titulo index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Listado Clientes</title>
     </head>
     <body>
         <h1>Listado de Clientes</h1>
